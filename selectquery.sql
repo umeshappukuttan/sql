767474107261d47b699ff1fb2e15ad2237f2ae5b
Umesh Appukuttan <u.appukuttantesting@gmail.com>
2019-07-01 08:47:30 +0530

    select-Uquery-proj

--- selectquery.sql
+++ selectquery.sql
@@ -3,3 +3,4 @@ SELECT name, population FROM world WHERE name IN ('Brazil', 'Russia', 'India', '
 SELECT name, area FROM world WHERE area BETWEEN 250000 AND 300000
 SELECT name, continent, population FROM world
 SELECT name FROM world WHERE population = 64105700
+SELECT name, LENGTH(name), continent, LENGTH(continent), capital, LENGTH(capital) FROM world WHERE name LIKE 'G%'
