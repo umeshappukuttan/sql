0088a6e1464528177c93f6db2fa3b8ad12051835
Umesh Appukuttan <u.appukuttantesting@gmail.com>
2020-01-02 08:47:30 +0530

    joinsquery-projworkv3

---
+++ joinsquery--projworkv3.sql
@@ -0,0 +1,18 @@
+SELECT *
+FROM table1 t1 JOIN table2 t2
+   ON t1.a = t2.c
+ORDER BY t1.a;
+GO
+
+SELECT *
+FROM table1 t1 JOIN table2 t2
+   ON t1.a = t2.c
+ORDER BY t1.a;
+GO
+
+SELECT *
+FROM table1 t1 LEFT OUTER JOIN table2 t2
+   ON t1.a = t2.c
+ORDER BY t1.a;
+GO
+
